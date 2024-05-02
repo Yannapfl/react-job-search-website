6358a37af1a256e4d50f1e4bb27cf809464927c6
yana_pfl <yanna.pvl24@gmail.com>
2024-05-02 13:02:17 +0300

    update block Skills

--- src/components/Content/Skills_block4/Skills.jsx
+++ src/components/Content/Skills_block4/Skills.jsx
@@ -1,16 +1,31 @@
 import './Skills.css';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../Cards_block3/Cards.css';
 
-
 export default function Skills({ data }) {
+    const [activeButtons, setActiveButtons] = useState([]);
+
+    const handleButtonClick = (index) => {
+        if (activeButtons.includes(index)) {
+            setActiveButtons(activeButtons.filter((item) => item !== index));
+        } else {
+            setActiveButtons([...activeButtons, index]);
+        }
+    }
+
     return (
         <div className='section'>
             <div className='text__block'>
                 <h3>Find your culture</h3>
             </div>
             <div className='btn-group-skills'>
-                {data.map((textButton) => <button key={textButton} type='button' className='button-skill'>{textButton}</button>)}
+                {data.map((textButton, idx) => <button 
+                key={idx} 
+                type='button' 
+                className={`button-skill ${ activeButtons.includes(idx) ? 'active-skill' : ''}`}
+                onClick={() => handleButtonClick(idx)}
+                >{textButton}</button>)}
             </div>
         </div>
     )
